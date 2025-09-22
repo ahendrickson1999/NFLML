cb2d3420dfbedfc61b04966331556d4feb3d57f0
ahendrickson1999 <ahendrickson@riversidemfg.com>
2025-09-21 21:12:30 -0400

    Change DEFAULT_SEASONS to only include 2020

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 # ---- CONFIG ----
-DEFAULT_SEASONS = [2020,2021,2022,2023,2024,2025]  # Start with one season. Add more as RAM allows.
+DEFAULT_SEASONS = [2020]  # Start with one season. Add more as RAM allows.
 ROLLING_WINDOW = 3
 
 # ---- LOAD DATA ----
